38e4549cad4183874998cf98988847898cd41722
Christoph M. Becker <cmbecker69@gmx.de>
2021-05-14 18:36:59 +0200

    tweaks

--- .github/workflows/build.ps1
+++ .github/workflows/build.ps1
@@ -5,10 +5,10 @@ param (
 
 $ErrorActionPreference = "Stop"
 
-if ($architecture -eq "x64") {
-    $platform = "x64"
-} else {
+if ($architecture -eq "x86") {
     $platform = "Win32"
+} else {
+    $platform = "x64"
 }
 
 if ($vs -eq "vc15") {
@@ -19,14 +19,14 @@ if ($vs -eq "vc15") {
     $toolset = "v142"
 }
 
-Set-Location builds\msvc\$slndir
-msbuild /t:Build /p:Configuration=StaticRelease /p:Platform=$platform libsodium.sln
-msbuild /t:Build /p:Configuration=DynRelease /p:Platform=$platform libsodium.sln
-Set-Location ..\..\..
-New-Item winlibs -ItemType "directory"
-xcopy bin\$platform\Release\$toolset\dynamic\libsodium.dll winlibs\bin\*
-xcopy bin\$platform\Release\$toolset\dynamic\libsodium.pdb winlibs\bin\*
-xcopy /e src\libsodium\include\*.h winlibs\include\*
-Remove-Item winlibs\include\sodium\private -Recurse
-xcopy bin\$platform\Release\$toolset\dynamic\libsodium.lib winlibs\lib\*
-Copy-Item bin\$platform\Release\$toolset\static\libsodium.lib winlibs\lib\libsodium_a.lib
+Set-Location "builds\msvc\$slndir"
+msbuild "/t:Build" "/p:Configuration=StaticRelease" "/p:Platform=$platform" "libsodium.sln"
+msbuild "/t:Build" "/p:Configuration=DynRelease" "/p:Platform=$platform" "libsodium.sln"
+Set-Location "..\..\.."
+New-Item "winlibs" -ItemType "directory"
+xcopy "bin\$platform\Release\$toolset\dynamic\libsodium.dll" "winlibs\bin\*"
+xcopy "bin\$platform\Release\$toolset\dynamic\libsodium.pdb" "winlibs\bin\*"
+xcopy /e "src\libsodium\include\*.h" "winlibs\include\*"
+Remove-Item "winlibs\include\sodium\private" -Recurse
+xcopy "bin\$platform\Release\$toolset\dynamic\libsodium.lib" "winlibs\lib\*"
+Copy-Item "bin\$platform\Release\$toolset\static\libsodium.lib" "winlibs\lib\libsodium_a.lib"
