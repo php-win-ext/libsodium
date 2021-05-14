8c5d28c52e677da668d557364250da44dd965236
Christoph M. Becker <cmbecker69@gmx.de>
2021-05-14 17:19:00 +0200

    Add vs parameter to build.ps1

--- .github/workflows/build.ps1
+++ .github/workflows/build.ps1
@@ -1,5 +1,6 @@
 param (
-    [Parameter(Mandatory)] $architecture
+    [Parameter(Mandatory)] $architecture,
+    [Parameter(Mandatory)] $vs
 )
 
 $ErrorActionPreference = "Stop"
@@ -10,14 +11,22 @@ if ($architecture -eq "x64") {
     $platform = "Win32"
 }
 
-Set-Location builds\msvc\vs2019
+if ($vs -eq "vc15") {
+    $slndir = "vs2017"
+    $toolset = "v141"
+} else {
+    $slndir = "vs2019"
+    $toolset = "v142"
+}
+
+Set-Location builds\msvc\$slndir
 msbuild /t:Build /p:Configuration=StaticRelease /p:Platform=$platform libsodium.sln
 msbuild /t:Build /p:Configuration=DynRelease /p:Platform=$platform libsodium.sln
 Set-Location ..\..\..
 New-Item winlibs -ItemType "directory"
-xcopy bin\$platform\Release\v142\dynamic\libsodium.dll winlibs\bin\*
-xcopy bin\$platform\Release\v142\dynamic\libsodium.pdb winlibs\bin\*
+xcopy bin\$platform\Release\$toolset\dynamic\libsodium.dll winlibs\bin\*
+xcopy bin\$platform\Release\$toolset\dynamic\libsodium.pdb winlibs\bin\*
 xcopy /e src\libsodium\include\*.h winlibs\include\*
 Remove-Item winlibs\include\sodium\private -Recurse
-xcopy bin\$platform\Release\v142\dynamic\libsodium.lib winlibs\lib\*
-Copy-Item bin\$platform\Release\v142\static\libsodium.lib winlibs\lib\libsodium_a.lib
+xcopy bin\$platform\Release\$toolset\dynamic\libsodium.lib winlibs\lib\*
+Copy-Item bin\$platform\Release\$toolset\static\libsodium.lib winlibs\lib\libsodium_a.lib
