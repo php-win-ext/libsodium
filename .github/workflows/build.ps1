24a55005531b856dd42668d32f5ba0e1fa0c1fc5
Christoph M. Becker <cmbecker69@gmx.de>
2021-05-14 16:37:54 +0200

    architecture matrix

--- .github/workflows/build.ps1
+++ .github/workflows/build.ps1
@@ -1,13 +1,21 @@
+param ([Parameter(Mandatory)] $architecture)
+
 $ErrorActionPreference = "Stop"
 
+if ($architecture -eq "x64") {
+    $platform = "x64"
+} else {
+    $platform = "Win32"
+}
+
 Set-Location builds\msvc\vs2019
-msbuild /t:Build /p:Configuration=StaticRelease /p:Platform=x64 libsodium.sln
-msbuild /t:Build /p:Configuration=DynRelease /p:Platform=x64 libsodium.sln
+msbuild /t:Build /p:Configuration=StaticRelease /p:Platform=$platform libsodium.sln
+msbuild /t:Build /p:Configuration=DynRelease /p:Platform=$platform libsodium.sln
 Set-Location ..\..\..
 New-Item winlibs -ItemType "directory"
-xcopy bin\x64\Release\v142\dynamic\libsodium.dll winlibs\bin\*
-xcopy bin\x64\Release\v142\dynamic\libsodium.pdb winlibs\bin\*
+xcopy bin\$platform\Release\v142\dynamic\libsodium.dll winlibs\bin\*
+xcopy bin\$platform\Release\v142\dynamic\libsodium.pdb winlibs\bin\*
 xcopy /e src\libsodium\include\*.h winlibs\include\*
 Remove-Item winlibs\include\sodium\private -Recurse
-xcopy bin\x64\Release\v142\dynamic\libsodium.lib winlibs\lib\*
-Copy-Item bin\x64\Release\v142\static\libsodium.lib winlibs\lib\libsodium_a.lib
+xcopy bin\$platform\Release\v142\dynamic\libsodium.lib winlibs\lib\*
+Copy-Item bin\$platform\Release\v142\static\libsodium.lib winlibs\lib\libsodium_a.lib
