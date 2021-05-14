e45456a031507d46a8aec6d8f0af43d8892212cb
Christoph M. Becker <cmbecker69@gmx.de>
2021-05-14 16:17:06 +0200

    Tweak build.ps1

--- .github/workflows/build.ps1
+++ .github/workflows/build.ps1
@@ -2,12 +2,10 @@ Set-Location builds\msvc\vs2019
 msbuild /t:Build /p:Configuration=StaticRelease /p:Platform=x64 libsodium.sln
 msbuild /t:Build /p:Configuration=DynRelease /p:Platform=x64 libsodium.sln
 Set-Location ..\..\..
-mkdir winlibs
+New-Item winlibs -ItemType "directory"
 xcopy bin\x64\Release\v142\dynamic\libsodium.dll winlibs\bin\*
 xcopy bin\x64\Release\v142\dynamic\libsodium.pdb winlibs\bin\*
 xcopy /e src\libsodium\include\*.h winlibs\include\*
-rmdir /s /q winlibs\include\sodium\private
-del winlibs\include\sodium\randombytes_nativeclient.h
+Remove-Item winlibs\include\sodium\private -Recurse
 xcopy bin\x64\Release\v142\dynamic\libsodium.lib winlibs\lib\*
-copy bin\x64\Release\v142\static\libsodium.lib winlibs\lib\libsodium_a.lib
-# Compress-Archive -Path winlibs\* -DestinationPath winlibs\libsodium-test-vs16-x64.zip
+Copy-Item bin\x64\Release\v142\static\libsodium.lib winlibs\lib\libsodium_a.lib
