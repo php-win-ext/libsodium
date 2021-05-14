2709805ac4cd988bad1a8db0530e201e32610437
Christoph M. Becker <cmbecker69@gmx.de>
2021-05-14 15:33:15 +0200

    assemble build artifacts

--- .github/workflows/build.ps1
+++ .github/workflows/build.ps1
@@ -1,2 +1,12 @@
 Set-Location builds\msvc\vs2019
 msbuild /t:Build /p:Configuration=StaticRelease /p:Platform=x64 libsodium.sln
+msbuild /t:Build /p:Configuration=DynRelease /p:Platform=x64 libsodium.sln
+Set-Location ..\..\..
+mkdir winlibs
+xcopy bin\x64\Release\v142\dynamic\libsodium.dll winlibs\bin\*
+xcopy bin\x64\Release\v142\dynamic\libsodium.pdb winlibs\bin\*
+xcopy /e src\libsodium\include\*.h winlibs\include\*
+rmdir /s /q winlibs\include\sodium\private
+del winlibs\include\sodium\randombytes_nativeclient.h
+xcopy bin\x64\Release\v142\dynamic\libsodium.lib winlibs\lib\*
+copy bin\x64\Release\v142\static\libsodium.lib winlibs\lib\libsodium_a.lib
